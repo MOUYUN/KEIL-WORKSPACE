02f5acd89e1e18d5f9c8366c56e6ba41b6d4890a
MOUYUN <yun_hebuter@outlook.com>
2021-04-16 15:43:11 +0800

    单片机学习
    独立按键完成

--- 独立按键/main.c
+++ 独立按键/main.c
@@ -1,16 +1,62 @@
 #include <REGX52.H>
+#include <INTRINS.H>
+
+void delay1s(void)   //?? 0us
+{
+    unsigned char a,b,c;
+    for(c=167;c>0;c--)
+        for(b=171;b>0;b--)
+            for(a=16;a>0;a--);
+    _nop_();  //if Keil,require use intrins.h
+}
+
+void delay500ms(void)   //?? 0us
+{
+    unsigned char a,b,c;
+    for(c=23;c>0;c--)
+        for(b=152;b>0;b--)
+            for(a=70;a>0;a--);
+}
+
+void delay100ms(void)   //?? 0us
+{
+    unsigned char a,b,c;
+    for(c=19;c>0;c--)
+        for(b=20;b>0;b--)
+            for(a=130;a>0;a--);
+}
+
+
+unsigned char number = 0;
 
 void main(){
+	
 
-//	P2 = 0xFE;
-	P2_0 = 0;
-	P2_1 = 0;
+	
 	while(1){
-		if(P3_1==0 && P3_0==0){
-			P2_0 = 0;
-		}else{
-			P2_0 = 1;
+		if(P3_1==0){
+			
+			delay100ms();
+			
+			number++;
+			P2 = ~number;
+			
+			if(number>=256){
+				number=0;
+			}
 		}
 	}
+
+//	P2 = 0xFE;
+//	P2_0 = 0;
+//	P2_1 = 0;
+//	while(1){
+//		if(P3_1==0 && P3_0==0){
+//			//delay1s();
+//			P2_0 = 0;
+//		}else{
+//			P2_0 = 1;
+//		}
+//	}
 	
 }
